e69426e163a8c72132220e02cb003a1e36b7185f
hiqua <quentin.hibon@polytechnique.edu>
2015-04-01 13:01:02 +0100

    .

--- Shannon.thy
+++ Shannon.thy
@@ -1,9 +1,19 @@
 theory Shannon
 imports Information
 begin
-typedef bit = "{0::nat,1}" apply auto done
-typedef 'a alphabet = "{A :: 'a set.(finite A \<and> \<not> ( A = {} ))}" apply auto done
-type_synonym data = int --"int alphabet"
+typedef bit = "{0,Suc 0}" by auto
+typedef letter = "{0::nat..10}" by auto
+
+fun alphabet :: "nat \<Rightarrow> nat set" where
+  "alphabet 0 = {}"|
+  "alphabet (Suc n) = {0::nat..n}"
+
+type_synonym word = "letter list"
+
+typedef code = "{D :: (letter list \<Rightarrow> letter list). 
+D [] = [] \<and> (\<forall>x. \<forall> xs. D (x#xs) = (D [x]) @ (D xs))}"
+by auto
+
 
 (* 
 X is the input, Y is the output.
@@ -11,38 +21,38 @@ They are not independent (if they are, all of this serves no purpose)
 We fix N, N' the measures (TODO: should I? Can we have two different bit measures?)
 The input is only correlated to the corresponding output.
 *)
+type_synonym prob = "letter \<Rightarrow> real"
 
-locale information_space_discrete = information_space +
-  fixes Input :: "nat \<Rightarrow> ('a \<Rightarrow> data)"
-  fixes Output :: "nat \<Rightarrow> ('a \<Rightarrow> data)"
-  fixes fi :: "data \<Rightarrow> real"
-  fixes fo :: "data \<Rightarrow> real"
-  fixes N :: "'a measure"
-  fixes N' :: "data measure"
-  assumes distr_i:
-    "simple_distributed N (Input i) fi"
-  assumes distr_o:
-    "simple_distributed N (Output i) fo"
-  assumes memoryless:
- "(m \<noteq> n \<longrightarrow> (indep_var N' (Input m) N' (Output n)) \<and> indep_var N' (Output m) N' (Output n))"
- assumes io_corr:
- "\<not> indep_var N' (Input n) N' (Output n)"
+locale information_space_discrete_second = information_space +
+fixes Input :: "nat \<Rightarrow> ('a \<Rightarrow> letter)"
+fixes Output :: "nat \<Rightarrow> ('a \<Rightarrow> letter)"
+fixes fi :: "prob"
+fixes fo :: "prob"
+fixes N :: "'a measure" --"we should take M?"
+fixes N' :: "letter measure"
+fixes C :: code
+(* fixes range *)
+assumes distr_i:
+  "simple_distributed N (Input i) fi"
+assumes distr_o:
+  "simple_distributed N (Output i) fo"
+assumes memoryless:
+"(m \<noteq> n \<longrightarrow> (indep_var N' (Input m) N' (Output n)) \<and> indep_var N' (Output m) N' (Output n))"
+assumes io_corr: (* this one is useless given m_info *)
+"\<not> indep_var N' (Input n) N' (Output n)"
+assumes m_info:
+  "\<I>(Input n ; Output n) > 0"
 
-context information_space_discrete
+context information_space_discrete_second
 begin
+lemma un: "simple_function N (Input i)"
+using distr_i simple_distributed_simple_function by blast
 
-(* fake lemma, just to get used to structured proofs *)
+lemma triv: "prob {a} \<le> 1" by auto
+
 (*
-lemma test: "prob_space.expectation N (Input i) = prob_space.expectation N (Input j)"
-proof -
-have 0: "i = j \<longrightarrow> prob_space.expectation N (Input i) = prob_space.expectation N (Input j)"
-by auto
-have 1: "i \<noteq> j \<longrightarrow>prob_space.expectation N (Input i) = prob_space.expectation N (Input j)"
-  proof
-    have 2: "density N (Input i) = density N (Input j)" using simple_distributed
-sorry
+fun codelength :: "code \<Rightarrow> prob \<Rightarrow> real" where
+  \<Sum>l \<in> (`space N)
 *)
-
-
 end
 end
