64ac2aa6804ad6685fbe4e2c5c9a595e6d497cbe
hiqua <quentin.hibon@polytechnique.edu>
2015-04-08 13:25:40 +0100

    format

--- Shannon.thy
+++ Shannon.thy
@@ -20,31 +20,31 @@ type_synonym bword = "bit list"
 
 (* this function cleans the dirty bword of all dummy characters *)
 fun d_clean :: "dbword \<Rightarrow> bword" where
-  "d_clean [] = []"|
-  "d_clean (B b # xs) = b # (d_clean xs)"|
-  "d_clean (D # xs) = d_clean xs"
+"d_clean [] = []"|
+"d_clean (B b # xs) = b # (d_clean xs)"|
+"d_clean (D # xs) = d_clean xs"
 
 type_synonym encode = "word \<Rightarrow> dbword"
 type_synonym decode = "dbword \<Rightarrow> word"
 type_synonym gcode = "encode * decode"
 (*
 fun binary_encode :: "word => dbword" where
-  "binary_encode 
+"binary_encode
 
 fun binary_decode :: "dbword \<Rightarrow> word" where
 
-typedef code = "{c :: gcode. \<forall>x y. (fst c) (snd c x) = x \<and> (snd c) (fst c y) = y}" 
-by 
+typedef code = "{c :: gcode. \<forall>x y. (fst c) (snd c x) = x \<and> (snd c) (fst c y) = y}"
+by
 auto
 *)
 (*
 fun lol :: "code \<Rightarrow> int" where
-  "lol c = 0"
+"lol c = 0"
 
 fun qsdf :: "code \<Rightarrow> encode" where
-  "qsdf c = fst c"
+"qsdf c = fst c"
 *)
-(* 
+(*
 X is the input, Y is the output.
 They are not independent (if they are, all of this serves no purpose)
 We fix N, N' the measures (TODO: should I? Can we have two different bit measures?)
@@ -62,13 +62,13 @@ fixes N :: "'a measure" --"we should take M?"
 fixes N' :: "letter measure"
 (* fixes range *)
 assumes distr_i:
-  "simple_distributed N (Input i) fi"
+"simple_distributed N (Input i) fi"
 assumes distr_o:
-  "simple_distributed N (Output i) fo"
+"simple_distributed N (Output i) fo"
 assumes memoryless:
 "(m \<noteq> n \<longrightarrow> (indep_var N' (Input m) N' (Output n)) \<and> indep_var N' (Output m) N' (Output n))"
 assumes m_info:
-  "\<I>(Input n ; Output n) > 0"
+"\<I>(Input n ; Output n) > 0"
 fixes nn::nat
 fixes kk::nat
 
@@ -80,7 +80,7 @@ using distr_i simple_distributed_simple_function by blast
 
 (*
 fun codelength :: "code \<Rightarrow> prob \<Rightarrow> real" where
-  \<Sum>l \<in> (`space N)
+\<Sum>l \<in> (`space N)
 *)
 end
 end
