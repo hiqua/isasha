e943148ba48273818ceeb2e547ad60238fd3697e
hiqua <quentin.hibon@polytechnique.edu>
2015-04-09 17:36:35 +0100

    update Shannon

--- Shannon.thy
+++ Shannon.thy
@@ -1,105 +1,88 @@
 theory Shannon
-imports Information "~~/src/HOL/Word/Word"
+imports Information
 begin
-(* typedef dbit = "{0::nat, 1, 2}" by auto*)
-typedef bit = "{True,False}" by auto
-datatype dbit = B bit | D
 
-(*typedef letter = "{0::nat..9}" by auto*)
-type_synonym letter = nat
-typedef ll = "{0::nat..<10}"
-by (metis atLeastLessThan_iff le0 zero_less_numeral)
+subsection{* Basic types and helpers *}
 
-
-(*
-Definition of the class of coded types, that is, types for which there is an
-encoding and decoding function.
-We assign a length to our type, this will be the length of the words we consider
-(constant).
-In practice, 'a will represent a D-ary alphabet, i.e. a finite set.
-For now, there are only guarantees of well-defined functions if the sizes
-match.
-What if they don't?
-*)
-
-class coded = len +
-fixes enc :: "'a list \<Rightarrow> bit list"
-fixes dec :: "bit list \<Rightarrow> 'a list"
-assumes "\<forall>x. (length x) = len_of TYPE ('a) \<Longrightarrow>  dec (enc x) = x"
-
-class block_coded = coded +
-fixes block_enc :: "'a list \<Rightarrow> bit list"
-fixes block_dec :: "bit list \<Rightarrow> 'a list"
-assumes "\<forall>x.\<forall>xs. (length x) = len_of TYPE('a) \<Longrightarrow>
-block_dec (block_enc (x @ xs)) = x @ block_dec (block_enc xs) \<and> block_enc x = enc x"
-
-type_synonym word = "letter list"
-type_synonym dbword = "dbit list"
+type_synonym bit = bool
 type_synonym bword = "bit list"
+type_synonym letter = nat
+type_synonym word = "letter list"
 
-(* this function cleans the dirty bword of all dummy characters *)
-fun d_clean :: "dbword \<Rightarrow> bword" where
-"d_clean [] = []"|
-"d_clean (B b # xs) = b # (d_clean xs)"|
-"d_clean (D # xs) = d_clean xs"
+type_synonym encoder = "word \<Rightarrow> bword"
+type_synonym decoder = "bword \<Rightarrow> word option"
+type_synonym code = "encoder * decoder"
 
-type_synonym encode = "word \<Rightarrow> dbword"
-type_synonym decode = "dbword \<Rightarrow> word"
-type_synonym gcode = "encode * decode"
-(*
-fun binary_encode :: "word => dbword" where
-"binary_encode
-
-fun binary_decode :: "dbword \<Rightarrow> word" where
-
-typedef code = "{c :: gcode. \<forall>x y. (fst c) (snd c x) = x \<and> (snd c) (fst c y) = y}"
-by
-auto
-*)
-(*
-fun lol :: "code \<Rightarrow> int" where
-"lol c = 0"
-
-fun qsdf :: "code \<Rightarrow> encode" where
-"qsdf c = fst c"
-*)
+subsection{* First locale, generic to both Shannon's theorems *}
 (*
 X is the input, Y is the output.
 They are not independent (if they are, all of this serves no purpose)
 We fix N, N' the measures (TODO: should I? Can we have two different bit measures?)
 The input is only correlated to the corresponding output.
 *)
+
 type_synonym prob = "letter \<Rightarrow> real"
 
-
-locale information_space_discrete_second = information_space +
+(* locale generic to both theorems *)
+locale information_space_discrete = information_space +
+(* channnel *)
 fixes Input :: "nat \<Rightarrow> ('a \<Rightarrow> letter)"
 fixes Output :: "nat \<Rightarrow> ('a \<Rightarrow> letter)"
 fixes fi :: "prob"
 fixes fo :: "prob"
 fixes N :: "'a measure" --"we should take M?"
 fixes N' :: "letter measure"
-(* fixes range *)
 assumes distr_i:
 "simple_distributed N (Input i) fi"
 assumes distr_o:
 "simple_distributed N (Output i) fo"
 assumes memoryless:
 "(m \<noteq> n \<longrightarrow> (indep_var N' (Input m) N' (Output n)) \<and> indep_var N' (Output m) N' (Output n))"
-assumes m_info:
+assumes mutual_info:
 "\<I>(Input n ; Output n) > 0"
-fixes nn::nat
-fixes kk::nat
+fixes source_entropy::real
+assumes entropy_defi : "source_entropy = \<H>(Input 0)"
 
-context information_space_discrete_second
+print_locale information_space
+
+(*
+TODO: generalize c::code, do not put in the locale. Have some predicates to
+allow reasonings about codes. Keep the input_block_size that limits the size of the input, and use it.
+*)
+(* We will generalize the type "code" to any input by splitting the input in piece of length below a constant *)
+subsection{* locale specific to the source coding theorem *}
+locale information_space_discrete_source = information_space_discrete +
+fixes input_block_size::nat
 begin
+
+definition lossless_code :: "code \<Rightarrow> bool"
+where
+ "lossless_code c = (\<forall>x. length x \<le> input_block_size \<longrightarrow> snd c (fst c x) = Some
+ x)"
+
+definition block_encoding_code :: "code
+\<Rightarrow> bool"
+where
+"block_encoding_code c = (\<forall>x. length x = input_block_size \<longrightarrow> (\<forall>xs. (fst c) (x @ xs) = (fst
+c) x @ (fst c) xs))"
+
+
+definition real_code ::
+"code \<Rightarrow> bool" where
+"real_code c = (lossless_code c \<and> block_encoding_code c)"
+
+
+definition code_rat :: "code \<Rightarrow> real"
+where
+"code_rat code = lebesgue_integral N (\<lambda>a. (fi ((Input 0) a)) * (length ((fst
+code) [(Input 0) a])))"
+
+lemma  (in information_space_discrete_source) rate_lower_bound : "source_entropy \<le> code_rate"
+sorry
+
+
 lemma un: "simple_function N (Input i)"
 using distr_i simple_distributed_simple_function by blast
 
-
-(*
-fun codelength :: "code \<Rightarrow> prob \<Rightarrow> real" where
-\<Sum>l \<in> (`space N)
-*)
 end
 end
