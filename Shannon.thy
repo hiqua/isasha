833b12bd4a987866a5ece5b444d08a5aa47b1386
hiqua <quentin.hibon@polytechnique.edu>
2015-04-16 12:59:53 +0100

    Update shannon

--- Shannon.thy
+++ Shannon.thy
@@ -185,7 +185,7 @@ ultimately show ?thesis using finite_k_words sum_vimage[where f=
 qed
 
 lemma am_maj :
-  "real (card (set_of_k_words_length_m c k m)) / b^m \<le> b^m / b^m"
+  "real (card (set_of_k_words_length_m c k m))  \<le> b^m "
 proof sorry
 
 
@@ -199,7 +199,8 @@ using am_maj[where c="c" and k="k" and m="m"] binary_space
 Groups_Big.setsum_mono[ where K="{1..<Suc(k*max_len c)}" and f="(\<lambda>m. real (card
 (set_of_k_words_length_m c k m))/b^m)"
 and g="\<lambda>m. b ^ m /b^m"]
- by (metis am_maj)
+ using  am_maj
+   Fields.linordered_field_inverse_zero_class.divide_le_cancel by simp
 have 1: "(\<Sum>m=1..<Suc(k * max_len c). b^m / b^m) = (\<Sum>m=1..<Suc(k
 *max_len c). 1)" using binary_space by auto
  have 2: "(\<Sum>m=1..<Suc(k*max_len c). 1) =  (k * max_len c)" using assms by simp
