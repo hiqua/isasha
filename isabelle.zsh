51758d91bae0e19dfef0ac462614ac1f592552bb
hiqua <quentin.hibon@polytechnique.edu>
2015-05-05 16:53:52 +0100

    update isabelle.zsh, add ro

--- isabelle.zsh
+++ isabelle.zsh
@@ -2,12 +2,12 @@
 # ISABELLE_BIN_PATH must contain the path to the Isabelle binary.
 isplore(){
   for f in $@; do
-    theory_name=`echo "$f" | sed 's/.thy//'`
     theory_name=`basename -s ".thy" "$f"`
-    new_theory_name=$theory_name"_tmp"
+    new_theory_name=$theory_name"_""`date +%s`"
     new_filename=$new_theory_name".thy"
     cp "$f" /tmp/$new_filename
-    sed -i'' "s/$theory_name/$new_theory_name/" /tmp/$new_filename
+    sed -i'' "s/ $theory_name$/ $new_theory_name/" /tmp/$new_filename
+    chmod u-w /tmp/$new_filename
     /.$ISABELLE_BIN_PATH /tmp/$new_filename
   done;
 }
