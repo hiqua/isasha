199232907929504ad05e6c77230fd0bb6a7d30f4
hiqua <quentin.hibon@polytechnique.edu>
2015-06-03 17:38:10 +0100

    isplore in background

--- isabelle.zsh
+++ isabelle.zsh
@@ -9,7 +9,7 @@ isplore(){
     cp "$f" /tmp/$new_filename
     sed -i'' "s/ $theory_name$/ $new_theory_name/" /tmp/$new_filename
     #chmod u-w /tmp/$new_filename
-    isabelle /tmp/$new_filename
+    isabelle /tmp/$new_filename &
     else
     echo "File "$f" unreadable"
   fi
