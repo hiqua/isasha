92bdcda5f04c250ca156dd6de8ab99d5bddeb005
hiqua <quentin.hibon@polytechnique.edu>
2015-06-04 14:03:25 +0100

    First attempt to complete source_code with vectors

--- Block_Source_Code.thy
+++ Block_Source_Code.thy
@@ -2,29 +2,40 @@ theory Block_Source_Code
 imports Source_Code
 begin
 locale block_source_code = information_space +
-  fixes fi :: "'b list \<Rightarrow> real"
-  fixes X::"'a \<Rightarrow> 'b list"
+  fixes fi :: "'b^'n \<Rightarrow> real"
+  fixes X::"'a \<Rightarrow> ('b^'n)"
   assumes distr_i: "simple_distributed M X fi"
   fixes H::real
   assumes b_val: "b = 2"
   assumes entropy_defi: "H = \<H>(X)"
 
-  fixes L :: "'b list set"
+  fixes L :: "('b^'n) set"
   assumes fin_L: "finite L"
   assumes emp_L: "L \<noteq> {}"
 
   assumes bounded_input: "X ` space M = L"
-  
-  fixes c::"'b list code"
-  assumes real_code : "((\<forall>x. snd c (fst c x) = Some x) \<and>
-(\<forall>w. (fst c) w = [] \<longleftrightarrow> w = []) \<and>
-(\<forall>x. fst c x = (fst c) [(hd x)] @ (fst c) (tl x)))"
 
-  fixes max_index::nat
-  assumes L_len: "\<And>x. x\<in>L \<Longrightarrow> length x = Suc max_index"
+  fixes c::"('b^'n) code"
+  assumes real_code : "((\<forall>x. snd c (fst c x) = Some x) \<and>
+    (\<forall>w. (fst c) w = [] \<longleftrightarrow> w = []) \<and>
+    (\<forall>x. fst c x = (fst c) [(hd x)] @ (fst c) (tl x)))"
+
+  (* distribution according a law *)
+  fixes f:: "'b \<Rightarrow> real"
+  assumes distr_comp: "\<And>i. simple_distributed M (\<lambda>a. (X a)$i) f"
+  (* independence *)
+  assumes indep: "\<forall>x. fi x = (\<Prod>i\<in>UNIV. f (x$i))"
+  fixes H_i::real
+  assumes H_i_def: "\<exists>i. H_i = \<H>(\<lambda>a. (X a)$i)"
+  fixes S
+  assumes space_img_1: "\<And>i. (\<lambda>a. (X a)$i) ` space M = S"
+
+print_locale information_space
+print_locale source_code
 
 
 sublocale block_source_code \<subseteq> source_code
+
 proof
 show "simple_distributed M X fi" by (simp add: distr_i)
 show "b = 2" by (simp add: b_val)
@@ -40,36 +51,58 @@ qed
 
 context block_source_code
 begin
-definition X_th :: "nat \<Rightarrow> 'a \<Rightarrow> 'b" where
-  "X_th n a = (X a) ! n"
+lemma "H \<le> cr" using rate_lower_bound by simp
 
 
+(* INTERESTING: not the same to have this def, and X_i i a = X a $ i *)
+definition X_i::"'n \<Rightarrow> 'a \<Rightarrow> 'b" where
+  "X_i i = (\<lambda>a. X a $ i)"
 
-end
+lemma space_img_2: "\<And>i. X_i i ` space M = S"
+by (simp add: space_img_1 X_i_def image_cong)
 
-locale block_source_code_link = block_source_code +
-  fixes L_u
-  assumes "L = {w. set w \<subseteq> L_u}"
-  fixes f
-  assumes "\<And>n. n \<le> max_index \<Longrightarrow> simple_distributed M (X_th n) f"
-begin
-lemma "\<And>bl. bl \<in> L \<Longrightarrow> fi bl = (\<Prod>i\<in>{1::nat..n}. f (bl!i))"
+theorems space_img = space_img_1 space_img_2
+
+lemma entropy_sum: "\<And>i. \<H>(X_i i) = - (\<Sum>x\<in>(X_i i)`space M. f x * log b (f x))"
+proof -
+fix i
+have "(\<lambda>a. X a $ i) =(X_i i)" using X_i_def[of i] by auto
+moreover hence "\<H>(\<lambda>a. X a $ i) = \<H>(X_i i)" by simp
+moreover have "\<H>(\<lambda>a. X a $ i) = - (\<Sum>x\<in>(\<lambda>a. X a $ i)`space M. f x * log b (f x))"
+using entropy_simple_distributed[OF distr_comp] by simp
+ultimately show "\<H>(X_i i) = - (\<Sum>x\<in>(X_i i)`space M. f x * log b (f x))" by simp
+qed
+
+lemma entropy_forall: "\<And>i j. \<H>(X_i i) = \<H>(X_i j)"
+using entropy_sum space_img
+proof -
+fix i j
+show " \<H>(X_i i) = \<H>(X_i j)"
+using entropy_sum space_img
+by simp
+qed
+
+lemma "\<And>i. H_i = \<H>(X_i i)"
+proof -
+fix i
+from H_i_def obtain j where "H_i = \<H>(\<lambda>a. X a $ j)" by blast
+hence "H_i = \<H>(X_i j)" using X_i_def by simp
+moreover have "\<H>(X_i j) = \<H>(X_i i)" using entropy_forall by simp
+ultimately show "H_i = \<H>(X_i i)" by simp
+qed
+
+
+lemma "\<forall>i. \<H>(X) = CARD('n) * H_i"
+using indep
 sorry
 
 
-lemma "\<H>(X) = Suc(max_index) * \<H>(X_th 0)"
+(* exists code for vectors such that code_rate \<le> H*)
+lemma "\<exists>co. code_rate co X \<le> H + 1"
 sorry
 
-lemma
-  fixes lengths::"'b \<Rightarrow> nat"
-  fixes letters::"'b set"
-  assumes fin_letters: "finite letters"
-  assumes kraft_ineq: "(\<Sum>i\<in>letters. 1/ b^(lengths i)) \<le> 1"
-  shows "\<exists>g::'b code. \<forall>x. x\<in> letters \<longrightarrow> length (fst g [x]) = lengths x \<and> inj_on (fst g) ((\<lambda>x. [x]) ` letters)"
-sorry
 
-lemma
-  shows "\<exists>g::'b code.
+(* exists code for scalars such that code_rate \<le> H/n*)
 
 end
 end
