2b5e9d920a03ef74883521195079c3871e9c950a
hiqua <quentin.hibon@polytechnique.edu>
2015-06-12 13:20:15 +0100

    big update

--- Block_Source_Code.thy
+++ Block_Source_Code.thy
@@ -14,11 +14,11 @@ locale block_source_code = information_space +
   assumes emp_L: "L \<noteq> {}"
 
   fixes L_enum :: "nat \<Rightarrow> 'b^'n"
-  assumes L_enum_bij: "bij_betw L_enum {1..card L} L"
-  assumes L_enum_dec: "\<And>i j. i\<in>{1..card L} \<Longrightarrow> j\<in>{1..card L} \<Longrightarrow> i \<le> j \<Longrightarrow>
+  assumes L_enum_bij: "bij_betw L_enum {0..card L - 1} L"
+  assumes L_enum_dec: "\<And>i j. i\<in>{0..card L - 1} \<Longrightarrow> j\<in>{0..card L - 1} \<Longrightarrow> i < j \<Longrightarrow>
     fi (L_enum i) \<ge> fi (L_enum j)"
 
-    assumes fi_pos: "\<And>x. x \<in> L \<Longrightarrow> 0 < fi x"
+  assumes fi_pos: "\<And>x. x \<in> L \<Longrightarrow> 0 < fi x"
 
 
   assumes bounded_input: "X ` space M = L"
@@ -120,12 +120,9 @@ definition llll::"int tree" where
 definition li::"'b^'n \<Rightarrow> nat" where
   "li x =   nat \<lceil>(log b (1/ fi x))\<rceil>"
 
+(* easy tedious *)
 lemma "\<And>x. x\<in>L \<Longrightarrow> li x = \<lceil>(log b (1/ fi x))\<rceil>"
 sorry
-
-
-print_context
-print_locale block_source_code
 
 lemma prb_space: "prob_space M"
 using emeasure_space_1 prob_spaceI by blast
@@ -133,8 +130,6 @@ using emeasure_space_1 prob_spaceI by blast
 
 lemma (in block_source_code) "\<And>x. x \<in> L \<Longrightarrow> fi x \<le> 1"
 using bounded_input Probability_Measure.prob_space.simple_distributed_setsum_space[OF prb_space, OF distr_i] fi_pos
-
-
 sorry
 
 lemma "\<And>x. x\<in> L \<Longrightarrow> 0 \<le> li x"
@@ -145,52 +140,142 @@ lemma "(\<Sum>x\<in>L. b powr (-li x)) \<le> 1"
 sorry
 
 
-(* define  function n_th_order that for an integer n from 1 to card S give the nth element with the
-order given by a certain function (the probability to occur, namely)
-*)
-(* done: L_enum *)
-
 definition L_enum_inv :: "'b^'n \<Rightarrow> nat" where
-  "L_enum_inv = the_inv_into {1..card L} L_enum"
+  "L_enum_inv = the_inv_into {0..card L - 1} L_enum"
 
 lemma L_enum_inv_inj:
-  "bij_betw L_enum_inv L {1..card L}" using bij_betw_the_inv_into[OF L_enum_bij]
+  "bij_betw L_enum_inv L {0..card L - 1}" using bij_betw_the_inv_into[OF L_enum_bij]
 by (simp add: L_enum_inv_def)
 
-lemma "\<And>l. l \<in> L \<Longrightarrow> li l \<le> li (L_enum (card L))"
+lemma "\<And>l. l \<in> L \<Longrightarrow> li l \<le> li (L_enum (card L - 1))"
 sorry
 
 definition kraft_sum_li ::"real" where
   "kraft_sum_li = (\<Sum>l\<in>L. 1 / b ^ li l)"
 
+definition kraft_sum_li_ineq :: "bool" where
+  "kraft_sum_li_ineq = (kraft_sum_li \<le> 1)"
+
+(* main three functions to do the encoding *)
+(* the lists are considered in reverse order, i.e. big [0] is a prefix of [1,0] *)
+fun next_list :: "bit list \<Rightarrow> bit list" where
+  "next_list [] = []"|
+  "next_list (x#xs) = (if x then False#(next_list xs) else True#xs)"
+
+fun next_list_n :: "bit list \<Rightarrow> nat \<Rightarrow> bit list" where
+  "next_list_n l 0 = l"|
+  "next_list_n l (Suc n) = next_list_n (next_list l) n"
+
+lemma "\<And>l. True \<in> set l \<Longrightarrow> l \<noteq> next_list l"
+by (metis length_greater_0_conv length_pos_if_in_set list.inject next_list.elims)
 
 
+fun pad :: "bit list \<Rightarrow> nat \<Rightarrow> bit list" where
+  "pad l 0 = l"|
+  "pad l (Suc n) = False#(pad l n)"
 
+fun encode :: "nat \<Rightarrow> (nat \<Rightarrow> nat) \<Rightarrow> bit list" where
+  "encode 0 len = pad [] (len 0)"|
+  "encode (Suc n) len = pad (next_list (encode n len)) (len (Suc n) - len n)"
 
-
-(* fun huffman_encoding :: "('b^'n) \<Rightarrow> nat list" where *)
-
-
-(* medium because of option tricks *)
-lemma "\<And>l. l\<in>L \<Longrightarrow> length (huffman_encoding l) = li l"
+(* easy *)
+lemma "\<And>n. length (pad l n) = length l + n"
 sorry
 
-
-lemma encoding_inj: "inj_on huffman_encoding L"
+lemma "\<And>l. next_list l \<noteq> l"
+(* easy *)
 sorry
 
-lemma "bij_betw huffman_encoding L (huffman_encoding`L)"
+(* is l1 a prefix of l2 *)
+fun is_prefix :: "'z list \<Rightarrow> 'z list \<Rightarrow> bool" where
+  "is_prefix l1 l2 = (if length l1 \<ge> length l2 then l1 = l2
+                                              else case l2 of [] \<Rightarrow> False | x#xs \<Rightarrow> is_prefix l1 xs)"
+
+lemma "\<not> is_prefix l1 l2 \<Longrightarrow> l1 \<noteq> l2"
+by auto
+
+lemma "\<And>l. length (next_list l) = length l"
+(* easy *)
+sorry
+
+fun huffman_encoding_u :: "('b^'n) \<Rightarrow> bit list" where
+  "huffman_encoding_u v = encode (L_enum_inv v) (\<lambda>n. li (L_enum n))"
+
+definition huffman_lists :: "bit list set" where
+  "huffman_lists = huffman_encoding_u ` L"
+
+subsection{* Order relation *}
+
+definition less_eq :: "bit list \<Rightarrow> bit list \<Rightarrow> bool" (infix "\<preceq>" 50) where
+  "less_eq l1 l2 = (\<exists>n. l2 = next_list_n l1 n)"
+
+definition less :: "bit list \<Rightarrow> bit list \<Rightarrow> bool" (infix "\<prec>" 50) where
+  "l1 \<prec> l2 = (l1 \<preceq> l2 \<and> l1 \<noteq> l2)"
+
+(* easy, how to use?
+just use lemmas from orderings.thy
+*)
+interpretation ordering less_eq less
+sorry
+
+(* example using this interpretation *)
+lemma "less_eq l1 l2 \<longleftrightarrow> less l1 l2 \<or> l1 = l2"
+by (simp add: order_iff_strict)
+
+(* hard *)
+lemma
+assumes kraft_sum_li_ineq
+shows "\<And>i j. j < card L \<Longrightarrow> i < j \<Longrightarrow> (huffman_encoding (L_enum i) \<prec> huffman_encoding (L_enum j))"
+sorry
+
+lemma "l1 \<prec> l2 \<Longrightarrow> \<not> is_prefix l1 l2"
+sorry
+
+theorem
+assumes kraft_sum_li_ineq
+shows "\<And>a b. a\<in>L \<Longrightarrow> b\<in>L \<Longrightarrow> a \<noteq> b \<Longrightarrow> \<not> is_prefix (huffman_encoding_u a) (huffman_encoding_u b)"
+sorry
+
+(* easy? *)
+lemma "\<And>l. l\<in>L \<Longrightarrow> length (huffman_encoding_u l) = li l"
+sorry
+
+(* easy *)
+lemma encoding_inj: "inj_on huffman_encoding_u L"
+sorry
+
+lemma "bij_betw huffman_encoding_u L (huffman_encoding_u`L)"
 using inj_on_imp_bij_betw[OF encoding_inj] by simp
 
-(* lemma exists the inverse function *)
+definition huffman_encoding_reverse_u :: "bit list \<Rightarrow> ('b^'n) option" where
+  "huffman_encoding_reverse_u x = (if x \<in> (huffman_encoding_u ` L) then Some (the_inv_into L huffman_encoding_u x) else None)"
+
+fun huffman_encoding_reverse_aux :: "bit list \<Rightarrow> bit list \<Rightarrow> ('b^'n) list option" where
+  "huffman_encoding_reverse_aux xs [] = (case huffman_encoding_reverse_u xs of None \<Rightarrow> None
+|Some res \<Rightarrow> (Some [res]))"|
+  "huffman_encoding_reverse_aux xs (y#ys) = (case huffman_encoding_reverse_u xs
+of None \<Rightarrow>
+huffman_encoding_reverse_aux (xs @ [y]) ys
+|Some res \<Rightarrow> (case huffman_encoding_reverse_aux [] ys
+  of None \<Rightarrow> None
+|Some res2 \<Rightarrow> Some (res # res2))
+)"
+
+definition huffman_encoding_reverse :: "bit list \<Rightarrow> ('b^'n) list option" where
+  "huffman_encoding_reverse xs = huffman_encoding_reverse_aux [] xs"
+
+definition huffman_encoding :: "('b^'n) list \<Rightarrow> bit list" where
+  "huffman_encoding xs = (fold (\<lambda>vl res. (huffman_encoding_u vl) @ res) xs [])"
 
 (* define the associated code *)
+definition huffman_code :: "('b^'n) code" where
+  "huffman_code = (huffman_encoding, huffman_encoding_reverse)"
 
+(* prove that this is a real code *)
 
+(* find the average length of this code *)
 
 (* exists code for vectors such that code_rate \<le> H*)
-lemma "\<exists>co. code_rate co X \<le> H + 1"
-sorry
 
 
 (* exists code for scalars such that code_rate \<le> H/n*)
